53aba9052facf8294cd0269f9983e8791a9e053c
datsuns <the.skeleton7@gmail.com>
2020-06-15 08:30:20 +0900

    set component in App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,34 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProfileCanvas
+      :height="parseInt(height)"
+      :width="parseInt(width)"
+      :foregroundColor="foregroundColor"
+      :backgroundColor="backgroundColor"
+      :text="text"
+      :size="parseInt(size)"
+      :font="font"
+      :baseline="baseline"
+      :angle="parseInt(angle)"
+      :isTransparent="isTransparent"
+      :baseImage="baseImage"
+      :checkImage="checkImage"
+      :profileImage="profileImage"
+      v-on:updated="updateDataURL($event)"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld    from './components/HelloWorld.vue'
+import ProfileCanvas from './components/ProfileCanvas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ProfileCanvas
   }
 }
 </script>
