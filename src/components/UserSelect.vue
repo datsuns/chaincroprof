681b2dcecd6413dbf04e69b80e4ee91dec770967
datsuns <the.skeleton7@gmail.com>
2020-06-27 18:03:18 +0900

    remove default

---
+++ src/components/UserSelect.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <select @change="$emit('select', $event.target.value)">
+      <option
+        v-for="optionItem in optionList"
+        :key="optionItem.name"
+        :value="optionItem.img"
+      >{{ optionItem.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserSelect',
+  props: {
+    labelText: String,
+    optionList: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
