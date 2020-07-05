eb5cc857c20dd541d5a13ac4289d75097c2c7d66
datsuns <the.skeleton7@gmail.com>
2020-07-05 10:10:38 +0900

    change footer style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <md-card class="md-primary" width="200px">
+    <md-card class="md-primary" md-theme="orange-card" >
       <md-card-header>
         <div class="md-title">ベースの画像は公式サイトのものを利用しています</div>
+        <md-button align="center" href="http://chronicle.sega-net.com/7th_chain/">公式サイトへGo!</md-button>
       </md-card-header>
 
-      <md-card-actions>
-        <md-button align="center" href="http://chronicle.sega-net.com/7th_chain/">公式サイトへ</md-button>
-      </md-card-actions>
-    </md-card>
-
-    <md-card>
       <md-card-content>
         <a href="https://twitter.com/datsuns" target="_blank">by @datsuns</a>
         <br>
         <a href="https://github.com/chainchroprof/prof7th" target="_blank">source code(github)</a>
+
+        <div class="md-subhead">更新履歴</div>
+        <div class="md-subhead">2020.07.05: フォント変えられまっせー</div>
+        <div class="md-subhead">2020.07.04: とりあえず作りましたぜー</div>
       </md-card-content>
     </md-card>
 
@@ -35,28 +34,20 @@
       <md-button class="md-primary md-raised" @click="showChangeLog = true">更新履歴</md-button>
     </md-card>
     -->
-    <md-card>
-      <md-card-content>
-        <div class="md-subhead">更新履歴</div>
-        <div class="md-subhead">2020.07.05: フォント変えられまっせー</div>
-        <div class="md-subhead">2020.07.04: とりあえず作りましたぜー</div>
-      </md-card-content>
-    </md-card>
   </div>
 </template>
 
 <script>
   export default {
     name: 'AppFooter',
-    data: () => ({
-      showChangeLog: false
-    })
+    //data: () => ({
+    //  showChangeLog: false
+    //})
   }
 </script>
 
 <style lang="scss" scoped>
   .md-card {
-    margin: 2px;
-    display: inline-block;
+    margin: 10px;
   }
 </style>
